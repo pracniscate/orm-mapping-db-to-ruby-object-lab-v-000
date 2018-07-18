555147179a089ef0c6095a15ee2526cd774b650f
Lorena <lorena.giddens@outlook.com>
2018-07-18 14:48:26 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -25,7 +25,7 @@ class Student
 
   def self.count_all_students_in_grade_9
     sql = <<-SQL
-      SELECT *
+      SELECT COUNT(*)
       FROM students
       WHERE grade = 9
     SQL
@@ -53,7 +53,6 @@ class Student
       FROM students
       WHERE grade = 10
       ORDER BY students.id
-      LIMIT ?
     SQL
 
     DB[:conn].execute(sql).map do |row|
